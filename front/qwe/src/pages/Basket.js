0efc0ed949c43696af5d593fd3d38be58ce32d92
rik51 <animeboy3228@outlook.com>
2022-12-01 02:28:59 +0700

    basket dodelal functional

--- front/qwe/src/pages/Basket.js
+++ front/qwe/src/pages/Basket.js
@@ -12,7 +12,7 @@ import {
     getBasketId,
     Plus
 } from "../http/DeviceAPI";
-import {observable, set, toJS} from "mobx";
+import {observable, set, toJS} from "mobx"
 import BasketItemsList from "../components/BasketItemsList";
 import {observer} from "mobx-react-lite";
 const Basket = observer(() => {
@@ -20,38 +20,45 @@ const Basket = observer(() => {
     const {user} = useContext(Context)
     let [dev,setDev] = useState(0)
     let [price,setPrice] = useState(0)
+    let [flagTxt,setFlagTxt] = useState(0)
     let cookie_req = document.cookie.split("=")
     let cookies = cookie_req[1]
     let summ=0
+    let oplataTxt1 = "Наличные "
+    let oplataTxt2 = "Безналичный "
+    let flag=0
     useEffect( () =>{
         getBasketDevices(cookies).then(data=>basket.setBasketsForBasket(data))
     },[user.Item])
-    basket.BasketsForBasket.map(basket=>{
+   basket.BasketsForBasket.map(basket=>{
         summ+=basket[0].finalPrice
     })
-    console.log(summ)
-    // useEffect( () =>{
-    //     basket.BasketsForBasket.map(basket=>console.log(toJS(basket[0].finalPrice)))
-    // },[user.Item])
-    //basket.BasketsForBasket.map(basket=>console.log(toJS(basket[0])))
-
-    // useEffect( () =>{
-    //     getBasketDevices(cookies).then(data=>{
-    //         basket.setBasketsForBasket(data)
-    //         basket.BasketsForBasket.map((basket)=>fetchOneDeviceList(basket.deviceListId).then(data=>setTheArray(prevState => [prevState,{currentOrNewKey:data}])))
-    //     })
-    // },[])
-    //console.log(toJS(basket.BasketsForBasket))
-    // console.log(theArray)
-
+    useEffect( () =>{
+        console.log(summ)
+        setPrice(summ)
+    },[basket.BasketsForBasket])
+    const twpProc = () => {
+        if(flag===0){
+            setPrice(summ)
+            setPrice(prevPrice => prevPrice*1.02)
+            flag=1
+            setFlagTxt(1)
+        }
+    }
+    const Def = () =>{
+        flag=0
+        setPrice(summ)
+        setFlagTxt(0)
+    }
+    console.log(flag)
     return (
         <Container>
             <Container className="oplata">
-                <button className="custom-btn btn-8">
-                    <span>1</span>
+                <button className="custom-btn btn-7" onClick={twpProc}>
+                    <span>Картой</span>
                 </button>
-                <button className="custom-btn btn-8">
-                    <span>1</span>
+                <button className="custom-btn btn-7" onClick={Def}>
+                    <span>Наличными</span>
                 </button>
             </Container>
             <Row>
@@ -59,9 +66,22 @@ const Basket = observer(() => {
                     <BasketItemsList dev={dev}/>
                 </Col>
             </Row>
-            <div className="basketFinal">
-                 Итого:{summ}
-            </div>
+            {
+                price===0 ?
+                <div className="basketFinal">
+                    Итого:{summ}
+                </div>
+                    :
+                    <div className="basketFinal">
+                        {
+                            flagTxt===0 ?
+                                <div>{oplataTxt1}</div>
+                                :
+                                <div>{oplataTxt2}</div>
+                        }
+                        Итого:{price}
+                    </div>
+            }
         </Container>
     );
 });
