54b3976f0835aa14bac4c28bb48ae77cc664eeda
rik51 <animeboy3228@outlook.com>
2023-01-01 18:47:00 +0700

    mainPage

--- front/qwe/src/pages/Main.js
+++ front/qwe/src/pages/Main.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import {Button} from "react-bootstrap";
 import {useNavigate} from "react-router-dom";
-import {checkCookie, CreateDeviceList, MainApi} from "../http/DeviceAPI";
-import axios from "axios";
+import "../styles/aboutComp.css"
+import Carouselzxc from "../components/Carousel";
 const Main = () => {
-    let qwe = document.cookie;
-    let zxc = qwe.slice("=")
     const  navigate = useNavigate()
     const click=()=>{
         navigate('/shop')
-        checkCookie(zxc[1]).then(console.log(document.cookie))
+
     }
     return (
         <div>
-            <Button onClick={click}></Button>
+            <Carouselzxc/>
+            <button
+                className="custom-btn btn-5"
+                onClick={click}>
+                <span style={{fontSize:"1.9em"}}>К КАТАЛОГУ ПРОДУКЦИИ</span>
+            </button>
+            <div className="aboutComp">
+                <div className="textComp">
+                    <div>
+                        <h1><b>О компании</b></h1>
+                    </div>
+                    <div>Мы предлагаем вам большой ассортимент различных пиломатериалов и материалов для строительства. Если не уверенны в выборевам с радосью помогут наши консультанты. Заказ так же можно оформить как на сайте и заказать доставку, так и посетить лично наш магазин и приобрести нужные вам материалы <br></br> Находимся в с.Ярково на улице</div>
+                </div>
+                <div className="imageComp"></div>
+            </div>
         </div>
     );
 };
