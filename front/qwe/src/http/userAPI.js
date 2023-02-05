fbd1f63456535fcf2f544b18f2d0735d5c67befb
rik51 <animeboy3228@outlook.com>
2023-02-06 01:27:05 +0700

    ?

--- front/qwe/src/http/userAPI.js
+++ front/qwe/src/http/userAPI.js
@@ -11,7 +11,7 @@ export const login = async (email, password) =>{
     localStorage.setItem('token', data.token)
     return jwtDecode(data.token)
 }
-export const check = async () =>{
+export const `check = async () =>{
     const {data} = await $authHost.get('api/user/auth')
     localStorage.setItem('token', data.token)
     return jwtDecode(data.token)
